bc4d0d060c78f7ea701aa0e60a9399a2ab96a503
wjr21 <wjr21@sussex.ac.uk>
2023-02-13 15:12:33 +0000

    Sped up parametric SED creation and adjusted plotting script

--- synthesizer/parametric/sfzh.py
+++ synthesizer/parametric/sfzh.py
@@ -5,10 +5,13 @@ import h5py
 import copy
 import numpy as np
 from scipy import integrate
+from scipy.interpolate import InterpolatedUnivariateSpline
 from unyt import yr
+import time
 
 
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 import cmasher as cmr
 
 from .. import exceptions
@@ -18,29 +21,72 @@ from ..plt import single_histxy, mlabel
 
 class BinnedSFZH:
 
-    """ this is a simple class for holding a binned star formation and metal enrichment history. This can be extended with other methods. """
+    """
+    This is a simple class for holding a binned star formation and metal
+    enrichment history. This can be extended with other methods.
+    """
 
-    def __init__(self, log10ages, metallicities, sfzh, sfh_f=None, Zh_f=None):
-        self.log10ages = log10ages
-        self.ages = 10**log10ages
-        self.log10ages_lims = [self.log10ages[0], self.log10ages[-1]]
-        self.metallicities = metallicities
-        self.metallicities_lims = [self.metallicities[0], self.metallicities[-1]]
-        self.log10metallicities = np.log10(metallicities)
-        self.log10metallicities_lims = [self.log10metallicities[0], self.log10metallicities[-1]]
+    def __init__(self, log10ages, metallicities, sfzh, sfh_f=None, Zh_f=None,
+                 parent_sfzh=None):
+
+        # Compute the individual star formation and metallicity histories
         self.sfzh = sfzh  # 2D star formation and metal enrichment history
         self.sfh = np.sum(self.sfzh, axis=1)  # 1D star formation history
         self.Z = np.sum(self.sfzh, axis=0)  # metallicity distribution
-        self.sfh_f = sfh_f  # function used to generate the star formation history if given
-        self.Zh_f = Zh_f  # function used to generate the metallicity history/distribution if given
 
-        # --- check if metallicities on regular grid in log10metallicity or metallicity or not at all (e.g. BPASS
+        # Assign ancillary data for this SFZH
+        if parent_sfzh is None:
+            self.log10ages = log10ages
+            self.ages = 10**log10ages
+            self.log10ages_lims = [self.log10ages[0], self.log10ages[-1]]
+            self.metallicities = metallicities
+            self.metallicities_lims = [
+                self.metallicities[0], self.metallicities[-1]]
+            self.log10metallicities = np.log10(metallicities)
+            self.log10metallicities_lims = [
+                self.log10metallicities[0], self.log10metallicities[-1]]
+
+            # Function used to generate the star formation history if given
+            self.sfh_f = sfh_f
+
+            # Function used to generate the metallicity history/distribution
+            # if given
+            self.Zh_f = Zh_f
+
+        else:
+
+            self._inherit_props(parent_sfzh)
+
+        # Check if metallicities on regular grid in log10metallicity or
+        # metallicity or not at all (e.g. BPASS)
         if len(set(self.metallicities[:-1]-self.metallicities[1:])) == 1:
             self.metallicity_grid = 'Z'
         elif len(set(self.log10metallicities[:-1]-self.log10metallicities[1:])) == 1:
             self.metallicity_grid = 'log10Z'
         else:
             self.metallicity_grid = None
+
+    def _inherit_props(self, parent):
+        """
+        A helper method to inherit properties from a parent SFZH and thus
+        avoid double up computations during addition.
+
+        Parameters
+        ----------
+        parent: obj (BinnedSFZH)
+            The parent SFZH object from which to inherit properties.
+        """
+
+        # Assign the inherited properties
+        self.log10ages = parent.log10ages
+        self.ages = parent.ages
+        self.log10ages_lims = parent.log10ages_lims
+        self.metallicities = parent.metallicities
+        self.metallicities_lims = parent.metallicities_lims
+        self.log10metallicities = parent.log10metallicities
+        self.log10metallicities_lims = parent.log10metallicities_lims
+        self.sfh_f = parent.sfh_f
+        self.Zh_f = parent.Zh_f
 
     def calculate_median_age(self):
         """ calculate the median age """
@@ -65,7 +111,8 @@ class BinnedSFZH:
         pstr += 'SUMMARY OF BINNED SFZH' + "\n"
         pstr += f'median age: {self.calculate_median_age().to("Myr"):.2f}' + "\n"
         pstr += f'mean age: {self.calculate_mean_age().to("Myr"):.2f}' + "\n"
-        pstr += f'mean metallicity: {self.calculate_mean_metallicity():.4f}' + "\n"
+        pstr += f'mean metallicity: {self.calculate_mean_metallicity():.4f}' + \
+            "\n"
         pstr += '-'*10 + "\n"
         return pstr
 
@@ -76,42 +123,59 @@ class BinnedSFZH:
 
             new_sfzh = self.sfzh + second_sfzh.sfzh
 
-            return BinnedSFZH(self.log10ages, self.metallicities, new_sfzh)
+            return BinnedSFZH(self.log10ages, self.metallicities, new_sfzh,
+                              sfh_f=self.sfh_f, Zh_f=self.Zh_f,
+                              parent_sfzh=self)
 
         else:
 
             exceptions.InconsistentAddition('SFZH must be the same shape')
 
-    def plot(self, show=True):
+    def plot(self, show=True, plot_sfh=False):
         """ Make a nice plots of the binned SZFH """
 
-        fig, ax, haxx, haxy = single_histxy()
+        # Define the figure grid
+        fig = plt.figure()
+        gs = fig.add_gridspec(nrows=2, ncols=2, wspace=0.0, hspace=0.0,
+                              height_ratios=[1, 4], width_ratios=[4, 1])
 
-        # this is technically incorrect because metallicity is not on a an actual grid.
-        ax.imshow(self.sfzh.T, origin='lower', extent=[
-                  *self.log10ages_lims, self.log10metallicities[0], self.log10metallicities[-1]], cmap=cmr.sunburst, aspect='auto')
+        # Define the axes
+        ax = fig.add_subplot(gs[1, 0])
+        haxy = fig.add_subplot(gs[1, 1])
+        haxx = fig.add_subplot(gs[0, 0])
+        haxx.axis('off')
+        haxy.axis('off')
 
-        # --- add binned Z to right of the plot
-        # haxx.step(log10ages, sfh, where='mid', color='k')
+        # This is technically incorrect because metallicity is not on a an
+        # actual grid.
+        ax.imshow(self.sfzh.T, origin='lower',
+                  extent=[
+                      *self.log10ages_lims,
+                      self.log10metallicities[0],
+                      self.log10metallicities[-1]
+                  ], cmap=cmr.sunburst, aspect='auto')
+
+        # Add binned Z to right of the plot
         haxy.fill_betweenx(self.log10metallicities, self.Z/np.max(self.Z),
                            step='mid', color='k', alpha=0.3)
 
-        # --- add binned SFH to top of the plot
-        # haxx.step(log10ages, sfh, where='mid', color='k')
+        # Add binned SFH to top of the plot
         haxx.fill_between(self.log10ages, self.sfh/np.max(self.sfh),
                           step='mid', color='k', alpha=0.3)
 
-        # --- add SFR to top of the plot
-        if self.sfh_f:
+        # Add SFR to top of the plot
+        if plot_sfh:
             x = np.linspace(*self.log10ages_lims, 1000)
             y = self.sfh_f.sfr(10**x)
             haxx.plot(x, y/np.max(y))
 
+        # Set axes limits
         haxy.set_xlim([0., 1.2])
-        haxy.set_ylim(self.log10metallicities_lims)
+        haxy.set_ylim(self.log10metallicities[0], self.log10metallicities[-1])
         haxx.set_ylim([0., 1.2])
         haxx.set_xlim(self.log10ages_lims)
 
+        # Set axis labels
         ax.set_xlabel(mlabel('log_{10}(age/yr)'))
         ax.set_ylabel(mlabel('log_{10}Z'))
 
@@ -159,11 +223,15 @@ def generate_sfzh(log10ages, metallicities, sfh, Zh, stellar_mass=1.):
 
     sfzh = np.zeros((len(log10ages), len(metallicities)))
 
+    # Evaluate sfrs
+    sfrs = sfh.sfr_arr(ages)
+
     if Zh.dist == 'delta':
+        f = InterpolatedUnivariateSpline(ages, sfrs, k=3)
         min_age = 0
         for ia, age in enumerate(ages[:-1]):
-            max_age = int(np.mean([ages[ia+1], ages[ia]]))  #  years
-            sf = integrate.quad(sfh.sfr, min_age, max_age)[0]
+            max_age = (ages[ia+1] + ages[ia]) / 2  # years
+            sf = f.integral(min_age, max_age)
             iZ = (np.abs(metallicities - Zh.Z(age))).argmin()
             sfzh[ia, iZ] = sf
             min_age = max_age
@@ -311,6 +379,16 @@ class SFH:
             else:
                 return 0.0
 
+        def sfr_arr(self, age):
+            """ age is lookback time """
+
+            okinds = age < self.duration
+
+            sfr = np.zeros(age.shape)
+            sfr[okinds] = 1.0
+
+            return sfr
+
     class Exponential(Common):
 
         """
@@ -371,3 +449,15 @@ class SFH:
                 return (1./(self.max_age-age))*np.exp(-(np.log(self.max_age-age)-self.T0)**2/(2*self.tau**2))
             else:
                 return 0.0
+
+        def sfr_arr(self, age):
+            """ age is lookback time """
+
+            okinds = age < self.max_age
+
+            sfr = np.zeros(age.shape)
+            sfr[okinds] = (1./(self.max_age-age[okinds])) * \
+                np.exp(-(np.log(self.max_age -
+                                age[okinds])-self.T0)**2/(2*self.tau**2))
+
+            return sfr
