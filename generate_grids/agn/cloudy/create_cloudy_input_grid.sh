7a711550816648d54903b7e7d8bf7445a46d0eeb
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-08 11:21:14 +0100

    updated script

--- generate_grids/agn/cloudy/create_cloudy_input_grid.sh
+++ generate_grids/agn/cloudy/create_cloudy_input_grid.sh
@@ -6,5 +6,5 @@ GRID_NAME="cloudy"
 CLOUDY_PARAMS="default.yaml"
 CLOUDY=$CLOUDY17
 
-python create_cloudy_input_grid.py -machine $MACHINE -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -grid_name $GRID_NAME -cloudy $CLOUDY -cloudy_params=$CLOUDY_PARAMS -dry_run True 
-# python create_cloudy_input_grid.py -machine $MACHINE -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -grid_name $GRID_NAME -cloudy $CLOUDY -cloudy_params=$CLOUDY_PARAMS
+# python create_cloudy_input_grid.py -machine $MACHINE -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -grid_name $GRID_NAME -cloudy $CLOUDY -cloudy_params=$CLOUDY_PARAMS -dry_run True 
+python create_cloudy_input_grid.py -machine $MACHINE -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -grid_name $GRID_NAME -cloudy $CLOUDY -cloudy_params=$CLOUDY_PARAMS
