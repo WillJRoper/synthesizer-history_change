dcff7e925c63b08543975ce84b78f530b0b752c5
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-08 11:19:09 +0100

    updated cloudy agn input grid creation code and scripts

--- generate_grids/agn/cloudy/create_cloudy_input_grid_basic.py
+++ generate_grids/agn/cloudy/create_cloudy_input_grid_basic.py
@@ -3,7 +3,6 @@ Given an SED (from an SPS model, for example), generate a cloudy atmosphere
 grid. Can optionally generate an array of input files for selected parameters.
 """
 
-import yaml
 import numpy as np
 from scipy import integrate
 from pathlib import Path
@@ -57,7 +56,11 @@ def load_cloudy_parameters(param_file='default.yaml', default_param_file='defaul
     # combine default parameters with 
     cloudy_params = default_cloudy_params | cloudy_params_
 
-    return cloudy_params
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -81,11 +84,13 @@ if __name__ == "__main__":
     cloudy_params = load_cloudy_parameters(param_file = args.cloudy_params)
 
     # read in the grid parameters
-    with open(args.grid_params, "r") as stream:
+    with open(grid_params, "r") as stream:
         try:
             grid_params = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
+
+
 
     if args.dry_run:
         print(machine)
@@ -93,27 +98,6 @@ if __name__ == "__main__":
         print(output_dir)
         print(cloudy)
         print(cloudy_params)
-        print(grid_params)
-        
-        
-        grid_axes = list(grid_params.keys())
-        print(grid_axes)
-
-        n_axes = len(grid_axes)
-
-        grid = np.array(np.meshgrid(*[np.array(v) for k,v in grid_params.items()]))
-
-        # determine number of models
-        n = 1
-        for dim in np.shape(grid): n *= dim
-        n /= len(grid)
-        n = int(n)
-
-        print(f'number of models to run: {n}')
-
-        grid_list = grid.T.reshape(n, n_axes)
-
-        print(grid_list)
 
     if not args.dry_run:
 
@@ -123,42 +107,62 @@ if __name__ == "__main__":
         # for submission system output files
         Path(f'{output_dir}/output').mkdir(parents=True, exist_ok=True)
 
-        
+        # log10TBB grid
+        log10TBBs = np.arange(4., 7., 0.2)
+
+        # metallicity grid
+        log10Zs = np.arange(-5., -1., 0.5)
+
+        # log10U
+        log10Us = np.array([-4., -3, -2, -1., 0., 1.])
+
+        # log10nH
+        log10n_Hs = np.array([0.,1.,2.,3.,4.])
+
+        # total number of models
+        N = len(log10Ts)*len(log10Zs)*len(log10Us)*len(log10n_Hs)
+
         # open the new grid
         with h5py.File(f'{synthesizer_data_dir}/grids/{grid_name}.hdf5', 'w') as hf:
 
             # add attribute with the grid axes
-            hf.attrs['grid_axes'] = grid_axes
+            hf.attrs['grid_axes'] = ['log10TBB', 'log10Z', 'log10U', 'log10n_H']
 
-            # add the grid axis values
-            for grid_axis in grid_axes:
-                hf[grid_axis] = grid_params[grid_axis]
-
-
-    for i, grid_params_ in enumerate(grid_list):
-    
-        grid_params = dict(zip(grid_axes, grid_params_))
-
-        params = cloudy_params | grid_params
-
-        print(params)
-
-        if not args.dry_run:
-
-            # create shape commands
-            TBB = 10**params['log10T']
-            cinputs = ShapeCommands.cloudy_agn(TBB)
             
-            # create input file
-            create_cloudy_input(str(i), shape_commands, abundances, output_dir=output_dir, **params)
+            hf['log10TBB'] = log10Ts
+            hf['log10Z'] = log10Zs
+            hf['log10U'] = log10Us
+            hf['log10n_H'] = log10n_Hs
 
-            # write out input file
-            with open(f"{output_dir}/input_names.txt", "a") as myfile:
-                myfile.write(f'{i}\n')
+
+        for iZ, log10Z in enumerate(log10Zs):
+            abundances = Abundances(10**log10Z)
+            for iT, log10T in enumerate(log10Ts):
+                for iU, log10U in enumerate(log10Us):
+                    for inH, log10n_H in enumerate(log10n_Hs):
         
-            if machine == 'apollo':
-                apollo_submission_script(N, output_dir, cloudy)
-            elif machine == 'cosma7':
-                cosma7_submission_script(N, output_dir, cloudy,
-                                        cosma_project='cosma7',
-                                        cosma_account='dp004')
+                        # cloudy_params
+
+                        cloudy_params['log10U'] = log10U
+                        cloudy_params['log10n_H'] = log10n_H
+
+                        # specify model_name
+                        model_name = f"{iT}_{iZ}_{iU}_{inH}"
+            
+                        # create shape commands
+                        TBB = 10**log10T
+                        cinputs = ShapeCommands.cloudy_agn(TBB)
+                        
+                        # create input file
+                        create_cloudy_input(model_name, shape_commands, abundances, output_dir=output_dir, **cloudy_params)
+
+                        # write out input file
+                        with open(f"{output_dir}/input_names.txt", "a") as myfile:
+                            myfile.write(f'{model_name}\n')
+        
+        if machine == 'apollo':
+            apollo_submission_script(N, output_dir, cloudy)
+        elif machine == 'cosma7':
+            cosma7_submission_script(N, output_dir, cloudy,
+                                    cosma_project='cosma7',
+                                    cosma_account='dp004')
