7254a5a6b69828834770177592a7594715c4f9c3
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-12 11:20:04 +0100

    updated code to get spectra and lines

--- generate_grids/agn/cloudy/create_synthesizer_grid.py
+++ generate_grids/agn/cloudy/create_synthesizer_grid.py
@@ -172,8 +172,9 @@ def add_spectra(grid_name, synthesizer_data_dir):
 
              # for an arbitrary grid, we should normalise by the bolometric luminosity of the incident spectra
             norm = np.trapz(spec_dict['incident'], x=nu)
-            print(indices, norm)
-            normalisation[*indices] = norm
+            print(indices, norm, type(indices))
+            print(normalisation[*indices])
+            # normalisation[*indices] = norm
 
             # save the normalised spectrum to the correct grid point 
             # for spec_name in spec_names:
