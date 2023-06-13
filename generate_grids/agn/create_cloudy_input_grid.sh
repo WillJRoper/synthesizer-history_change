29def2e2317e693f94732e5f75f804738661f382
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-13 13:22:53 +0100

    updated script

--- generate_grids/agn/create_cloudy_input_grid.sh
+++ generate_grids/agn/create_cloudy_input_grid.sh
@@ -6,4 +6,6 @@ GRID_NAME="agn_cloudy_T_alpha_Z_U_nH"
 # GRID_NAME="agn_feltre16_alpha_Z_U_nH" 
 CLOUDY=$CLOUDY17
 
+echo "python create_cloudy_input_grid.py -machine $MACHINE -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -cloudy $CLOUDY -grid_name "
+
 python create_cloudy_input_grid.py -machine $MACHINE -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -grid_name $GRID_NAME -cloudy $CLOUDY
