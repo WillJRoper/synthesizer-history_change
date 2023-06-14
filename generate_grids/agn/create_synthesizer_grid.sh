2a4f8fab156e1e1de640432740644b15fa43acce
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-06-14 10:22:45 +0100

    added loop over grids.txt

--- generate_grids/agn/create_synthesizer_grid.sh
+++ generate_grids/agn/create_synthesizer_grid.sh
@@ -1,8 +1,11 @@
 
 SYNTHESIZER_DATA_DIR="/research/astrodata/highz/synthesizer/"
-GRID_NAME="agn_cloudy_T_alpha_Z_U_nH" 
-# GRID_NAME="agn_feltre16_alpha_Z_U_nH" 
+while read GRID_NAME; do
+  echo "$GRID_NAME"
+  python create_synthesizer_grid.py -grid_name $GRID_NAME -synthesizer_data_dir $SYNTHESIZER_DATA_DIR
+done < grids.txt
 
-python create_synthesizer_grid.py -grid_name $GRID_NAME -synthesizer_data_dir $SYNTHESIZER_DATA_DIR
+
+
 
  
